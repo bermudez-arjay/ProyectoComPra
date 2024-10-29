97d3b7c7da099e83cc40777eec111f230a1af819
ALISVETH CENTENO <ALISVETH CENTENO@STARBYUL-05>
2024-10-29 16:05:59 -0600

    add changes Pilas

---
+++ Pilas/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Pilas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnFinslizarCompra = new System.Windows.Forms.Button();
+            this.dgvCarrito = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtCantidadProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecioProducto = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPrecioProducto);
+            this.groupBox1.Controls.Add(this.txtCantidadProducto);
+            this.groupBox1.Controls.Add(this.txtNombreProducto);
+            this.groupBox1.Controls.Add(this.btnSalir);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 276);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comprs de Prductos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txttotal);
+            this.groupBox2.Controls.Add(this.dgvCarrito);
+            this.groupBox2.Controls.Add(this.btnFinslizarCompra);
+            this.groupBox2.Location = new System.Drawing.Point(219, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(569, 275);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Carrito de Compras";
+            // 
+            // btnFinslizarCompra
+            // 
+            this.btnFinslizarCompra.Location = new System.Drawing.Point(476, 245);
+            this.btnFinslizarCompra.Name = "btnFinslizarCompra";
+            this.btnFinslizarCompra.Size = new System.Drawing.Size(75, 23);
+            this.btnFinslizarCompra.TabIndex = 0;
+            this.btnFinslizarCompra.Text = "Comprar";
+            this.btnFinslizarCompra.UseVisualStyleBackColor = true;
+            this.btnFinslizarCompra.Click += new System.EventHandler(this.btnFinslizarCompra_Click);
+            // 
+            // dgvCarrito
+            // 
+            this.dgvCarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarrito.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvCarrito.Location = new System.Drawing.Point(6, 19);
+            this.dgvCarrito.Name = "dgvCarrito";
+            this.dgvCarrito.Size = new System.Drawing.Size(557, 220);
+            this.dgvCarrito.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(119, 246);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Precio :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cantdad :";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(13, 246);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(45, 36);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(149, 20);
+            this.txtNombreProducto.TabIndex = 6;
+            // 
+            // txtCantidadProducto
+            // 
+            this.txtCantidadProducto.Location = new System.Drawing.Point(45, 122);
+            this.txtCantidadProducto.Name = "txtCantidadProducto";
+            this.txtCantidadProducto.Size = new System.Drawing.Size(149, 20);
+            this.txtCantidadProducto.TabIndex = 7;
+            this.txtCantidadProducto.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtPrecioProducto
+            // 
+            this.txtPrecioProducto.Location = new System.Drawing.Point(45, 199);
+            this.txtPrecioProducto.Name = "txtPrecioProducto";
+            this.txtPrecioProducto.Size = new System.Drawing.Size(149, 20);
+            this.txtPrecioProducto.TabIndex = 8;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(370, 246);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(100, 20);
+            this.txttotal.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Cantidad";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Precio";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFinslizarCompra;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvCarrito;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtCantidadProducto;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtPrecioProducto;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Pilas/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pilas
+{
+    public partial class Form1 : Form
+    {
+        private Stack<Producto> stackProductos;
+        decimal total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            stackProductos = new Stack<Producto>();
+
+            btnFinslizarCompra.Enabled = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombreProducto.Text;
+            decimal precio;
+            int cantidad;
+
+            if(!decimal.TryParse(txtPrecioProducto.Text, out precio) || precio <= 0)
+            {
+                MessageBox.Show("Ingree un Precio Valido");
+                return;
+            }
+            if (!int.TryParse(txtCantidadProducto.Text, out cantidad) || cantidad <= 0)
+            {
+                MessageBox.Show("Ingrese un Cantidad Validad");
+                return;
+            }
+
+            Producto producto = new Producto(nombre,precio,cantidad);
+            stackProductos.Push(producto);
+
+            dgvCarrito.Rows.Add(producto.Nombre, producto.Cantidad, producto.Precio);
+
+         
+
+                total += producto.Precio * producto.Cantidad;
+
+            txttotal.Text = Convert.ToString(total);
+            LimpiarCampos();
+            btnFinslizarCompra.Enabled = true;
+        }
+
+
+        private void btnFinslizarCompra_Click(object sender, EventArgs e)
+        {
+            decimal total = 0;
+
+
+            while (stackProductos.Count > 0)
+            {
+              Producto  producto = stackProductos.Pop();
+
+                total += producto.Precio * producto.Cantidad;
+            }
+            MessageBox.Show($"El Total de su compra fue de ${total}");
+            MessageBox.Show("Gracias Por su compra");
+
+            dgvCarrito.Rows.Clear();
+            txttotal.Clear();
+            btnFinslizarCompra.Enabled = false;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombreProducto.Clear();
+            txtPrecioProducto.Clear();
+            txtCantidadProducto.Clear();
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
